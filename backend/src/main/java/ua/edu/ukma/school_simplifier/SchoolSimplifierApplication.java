017f56816b7028fc0db29c95273d55286710ecf7
Victor <slavco2.p@gmail.com>
2022-03-13 20:45:06 +0200

    Starting project setup

---
+++ backend/src/main/java/ua/edu/ukma/school_simplifier/SchoolSimplifierApplication.java
@@ -0,0 +1,13 @@
+package ua.edu.ukma.school_simplifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolSimplifierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolSimplifierApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/ua/edu/ukma/school_simplifier/SchoolSimplifierApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.edu.ukma.school_simplifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolSimplifierApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
