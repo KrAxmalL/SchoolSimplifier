a5f15c0f1bad36275736e1c318220cb07f4e219b
Victor <slavco2.p@gmail.com>
2022-05-17 19:38:59 +0300

    Move jwt algorithm secret to properties file

--- backend/src/main/java/ua/edu/ukma/school_simplifier/security/jwt/JWTManager.java
+++ backend/src/main/java/ua/edu/ukma/school_simplifier/security/jwt/JWTManager.java
@@ -4,6 +4,8 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 import ua.edu.ukma.school_simplifier.domain.security.Principal;
@@ -26,9 +28,8 @@ public class JWTManager {
     private final Algorithm algorithm;
     private final JWTVerifier verifier;
 
-    public JWTManager() {
-        //todo: move secret to properties
-        this.algorithm = Algorithm.HMAC256("secret".getBytes(StandardCharsets.UTF_8));
+    public JWTManager(@Value("${jwt.algorithm.secret}") String algorithmSecret) {
+        this.algorithm = Algorithm.HMAC256(algorithmSecret.getBytes(StandardCharsets.UTF_8));
         this.verifier = JWT.require(algorithm).build();
     }
 
