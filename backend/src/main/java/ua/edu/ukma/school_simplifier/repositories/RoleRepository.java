c73fb47810be60f59bb113a2169cf88673677630
Victor <slavco2.p@gmail.com>
2022-05-17 16:32:11 +0300

    Change query for getting role by name

--- backend/src/main/java/ua/edu/ukma/school_simplifier/repositories/RoleRepository.java
+++ backend/src/main/java/ua/edu/ukma/school_simplifier/repositories/RoleRepository.java
@@ -3,11 +3,10 @@ package ua.edu.ukma.school_simplifier.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import ua.edu.ukma.school_simplifier.models.security.Role;
+import ua.edu.ukma.school_simplifier.domain.security.Role;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
-    @Query(value = "SELECT * FROM role WHERE name = :roleName",
-            nativeQuery=true)
+    @Query(value = "FROM Role WHERE roleName = :roleName")
     Role findRoleByName(@Param("roleName") String roleName);
 }
