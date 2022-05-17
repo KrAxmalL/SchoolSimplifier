5229923f2e36dc042473a6bd2714b1c9080493aa
Victor <slavco2.p@gmail.com>
2022-05-17 21:38:42 +0300

    Add basic layout

--- frontend/src/layout/Header.js
+++ frontend/src/layout/Header.js
@@ -3,8 +3,15 @@ import classes from './Header.module.css';
 function Header() {
     return (
         <header className={classes.header}>
-            <div className={classes['header-left']}>Header Left</div>
-            <div className={classes['header-right']}>Header Right</div>
+            <div className={classes['header-left']}>
+                <div>
+                    <span></span>
+                    <h2>Menu</h2>
+                </div>
+            </div>
+            <div className={classes['header-right']}>
+                <h1 className={classes['header-title']}>School Simplifier</h1>
+            </div>
         </header>
     );
 }
