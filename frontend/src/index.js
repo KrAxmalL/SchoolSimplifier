11c66ecbb931478e3ab1a3cfe6f7f391ee28be67
Victor <slavco2.p@gmail.com>
2022-05-17 22:48:32 +0300

    Add loading tokens from local storage on startup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
 import store from './store';
+import { authActions } from './store/auth-slice';
 import { Provider } from 'react-redux';
-
+import App from './App';
 import './index.css';
+
+store.dispatch(authActions.loadTokensFromStorage());
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
