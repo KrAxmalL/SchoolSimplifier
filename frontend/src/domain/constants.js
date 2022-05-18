0211c70e7414fc6a0709fccd2d2803ce1976b7f0
Victor <slavco2.p@gmail.com>
2022-05-18 19:43:48 +0300

    Define constants to be used later

---
+++ frontend/src/domain/constants.js
@@ -0,0 +1,15 @@
+
+export const Roles = {
+    STUDENT: "STUDENT",
+    TEACHER: "TEACHER"
+};
+
+export const Days = {
+    MONDAY: "Понеділок",
+    TUESDAY: "Вівторок",
+    WEDNESDAY: "Середа",
+    THURSDAY: "Четвер",
+    FRIDAY: "П'ятниця",
+    SATURDAY: "Субота",
+    SUNDAY: "Неділя"
+};
