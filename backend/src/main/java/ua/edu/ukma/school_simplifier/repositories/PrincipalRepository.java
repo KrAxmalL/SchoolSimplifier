41a702bcfed6087f2707c34f2565f3d376324016
Victor <slavco2.p@gmail.com>
2022-05-18 12:58:14 +0300

    Move JwtToken repository functions to principal repository

--- backend/src/main/java/ua/edu/ukma/school_simplifier/repositories/PrincipalRepository.java
+++ backend/src/main/java/ua/edu/ukma/school_simplifier/repositories/PrincipalRepository.java
@@ -1,13 +1,32 @@
 package ua.edu.ukma.school_simplifier.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import ua.edu.ukma.school_simplifier.domain.security.Principal;
+
+import java.util.Optional;
 
 public interface PrincipalRepository extends JpaRepository<Principal, Long> {
 
     @Query(value = "FROM Principal u WHERE u.email = :email")
     Principal findPrincipalByEmail(@Param("email") String email);
 
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query(value = "DELETE FROM principal_jwt WHERE principal_id = :target_principal_id",
+            nativeQuery = true)
+    void deleteTokenForPrincipal(@Param("target_principal_id") Long principalId);
+
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query(value = "INSERT INTO principal_jwt(principal_id, token) " +
+                   "VALUES(:target_principal_id, :target_token)",
+            nativeQuery = true)
+    void addTokenForPrincipal(@Param("target_principal_id") Long principalId,
+                                 @Param("target_token") String refreshToken);
+
+    @Query(value = "SELECT token FROM principal_jwt " +
+                   "WHERE token = :target_token",
+            nativeQuery = true)
+    Optional<String> getRefreshToken(@Param("target_token") String refreshToken);
 }
--- backend/src/main/java/ua/edu/ukma/school_simplifier/services/JwtTokenServiceImpl.java
+++ backend/src/main/java/ua/edu/ukma/school_simplifier/services/JwtTokenServiceImpl.java
@@ -10,11 +10,9 @@ import org.springframework.transaction.annotation.Transactional;
 import ua.edu.ukma.school_simplifier.domain.security.TokenPair;
 import ua.edu.ukma.school_simplifier.exceptions.InvalidTokenException;
 import ua.edu.ukma.school_simplifier.exceptions.TokenNotFoundException;
-import ua.edu.ukma.school_simplifier.domain.security.JwtToken;
 import ua.edu.ukma.school_simplifier.domain.security.Principal;
-import ua.edu.ukma.school_simplifier.repositories.JWTRepository;
+import ua.edu.ukma.school_simplifier.repositories.PrincipalRepository;
 import ua.edu.ukma.school_simplifier.security.jwt.JWTManager;
-import ua.edu.ukma.school_simplifier.utils.Pair;
 
 import java.util.Date;
 import java.util.List;
@@ -25,17 +23,19 @@ import java.util.Optional;
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class JwtTokenServiceImpl implements JwtTokenService {
 
-    private final JWTRepository jwtRepository;
     private final JWTManager jwtManager;
 
     private final PrincipalService principalService;
+    private final PrincipalRepository principalRepository;
 
     @Override
     public TokenPair getNewTokens(Principal principal) {
         String accessToken = jwtManager.getAccessToken(principal);
         String refreshToken = jwtManager.getRefreshToken(principal);
 
-        jwtRepository.save(new JwtToken(refreshToken, principal.getId()));
+        final Long principalId = principal.getId();
+        principalRepository.deleteTokenForPrincipal(principalId);
+        principalRepository.addTokenForPrincipal(principalId, refreshToken);
 
         return new TokenPair(accessToken, refreshToken);
     }
@@ -43,7 +43,7 @@ public class JwtTokenServiceImpl implements JwtTokenService {
     @Override
     public TokenPair getRefreshedTokens(String refreshToken) {
         try {
-            final Optional<JwtToken> prevRefreshToken = jwtRepository.findById(refreshToken);
+            final Optional<String> prevRefreshToken = principalRepository.getRefreshToken(refreshToken);
             if (prevRefreshToken.isPresent()) {
                 final String email = getEmail(refreshToken);
                 final Principal principal = principalService.getPrincipal(email);
