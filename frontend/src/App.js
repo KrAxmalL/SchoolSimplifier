d96b7e1d1689e1c6233a683e1da25b1db4fdcf2f
Victor <slavco2.p@gmail.com>
2022-05-18 12:18:11 +0300

    Add RequireAuth and layouts for teacher and student routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,11 @@ import NotFound from './pages/public/NotFound';
 import './App.css';
 import { useSelector } from 'react-redux';
 import { getHomePageForUser } from './utils/navigation';
+import RequireAuth from './components/security/RequireAuth';
+import StudentSchedule from './pages/student/StudentSchedule';
+import TeacherSchedule from './pages/teacher/TeacherSchedule';
+import StudentLayout from './components/student/StudentLayout';
+import TeacherLayout from './components/teacher/TeacherLayout';
 
 function App() {
   const roles = useSelector(state => state.auth.roles);
@@ -16,6 +21,18 @@ function App() {
         <Route path='/' exact element={<Navigate redirect to={getHomePageForUser(roles)} />}></Route>
         <Route path='/login' exact element={<Login />}></Route>
         <Route path='/unauthorized' exact element={<Unauthorized />}></Route>
+
+        <Route path='/student' element={<RequireAuth allowedRoles={['STUDENT']} />}>
+          <Route element={<StudentLayout />}>
+            <Route path='schedule' element={<StudentSchedule />}></Route>
+          </Route>
+        </Route>
+
+        <Route path='/teacher' element={<RequireAuth allowedRoles={['TEACHER']} />}>
+          <Route element={<TeacherLayout />}>
+            <Route path='schedule' element={<TeacherSchedule />}></Route>
+          </Route>
+        </Route>
 
         <Route path='*' element={<NotFound />}></Route>
       </Routes>
