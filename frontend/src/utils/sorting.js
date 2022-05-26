b85ffcc8c325802ef559ab7c53df00a0f97ab1d3
Victor <slavco2.p@gmail.com>
2022-05-26 17:45:15 +0300

    add sorting utils

---
+++ frontend/src/utils/sorting.js
@@ -0,0 +1,23 @@
+
+export function sortStudentsByInitials(students) {
+    return students.sort((stud1, stud2) => {
+        let compareRes = stud1.lastName.localeCompare(stud2.lastName);
+        if(compareRes === 0) {
+            compareRes = stud1.firstName.localeCompare(stud2.firstName);
+            if(compareRes === 0) {
+                compareRes = stud1.patronymic.localeCompare(stud2.patronymic);
+            }
+        }
+        return compareRes;
+    });
+};
+
+export function sortScheduleRecords(scheduleRecords) {
+    return scheduleRecords.sort((rec1, rec2) => {
+        let compareRes = rec1.lessonNumber < rec2.lessonNumber;
+        if(compareRes === 0) {
+            compareRes = rec1.groupNumber < rec2.groupNumber;
+        }
+        return compareRes;
+    });
+};
